1372a87d79d678c61e7a95d3899b507181548328
Yevhenii Bykov <Yevhenii_Bykov@epam.com>
2020-09-07 21:46:36 +0300

    play dotka

--- proggram.py
+++ proggram.py
@@ -1,3 +1,6 @@
+import random
+from time import sleep
+import webbrowser
 import sys
 
 yes = {'yes','y', ''}
@@ -7,19 +10,30 @@ year = 2020
 print('How old are you? ', end='')
 my_age = input()
 
-print('How old are your father? ', end='')
-father_age = input()
-
-age_of_your_born = float(father_age) - float(my_age)
-print('You born when your father was: {0}'.format(age_of_your_born))
-
 print('You from Russia? [y/n]: ', end='')
 choice = input().lower()
 if choice in yes:
-    print('How old are your father? ', end='')
-    father_age = input()
+    your_age_of_dead = 77 - float(my_age) + random.randint(1,3)
+    print('You will still live approximately: {0}'.format(your_age_of_dead))
 elif choice in no:
     sys.stdout.write("Migrate to Russia")
 else:
     sys.stdout.write("Please respond with 'yes' or 'no'")
+    exit(1)
 
+print('Processing your data... ')
+
+for i in range(21):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
+    sys.stdout.flush()
+    sleep(0.25)
+
+print(' ')
+if float(my_age) >= 18:
+    print('You must drink vodka and play dotka')
+    webbrowser.open('steam://rungameid/570', new=1)
+elif float(my_age) <= 0:
+    print('Lie is for bitches')
+else:
+    print('You must study at school')
