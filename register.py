6e3f91afbb5520f844ef09aa965f37afebbcbdd0
Yevhenii Bykov <yevhenii_bykov@epam.com>
2020-10-04 01:39:43 +0300

    updated output

--- register.py
+++ register.py
@@ -18,7 +18,6 @@ def watermark_text(input_image_path,
     black = (3, 8, 12)
     font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 50)
     drawing.text(pos, text, fill=black, font=font)
-    photo.show()
     photo.save(output_image_path)
 
 if __name__ == '__main__':
