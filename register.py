04fd0501a337be74ac61538e67e013222e46b544
Yevhenii Bykov <yevhenii_bykov@epam.com>
2020-10-04 01:44:25 +0300

    updated output

--- register.py
+++ register.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     print('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░')
     Tk().withdraw()
     img = askopenfilename(filetypes=[('PNG pictures','*.png'), ('JPEG pictures','*.jpg')], title= "Please select a Image")
-    ans = input("Water mark text: ")
+    ans = input("Water mark text: ") + ' (c) Master Daniil'
     output_file = input("File name: ") + '.png'
     for i in tqdm(range(100)):
       time.sleep(0.04)
