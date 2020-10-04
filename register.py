a7a7de7671b9cca2d86e36e34c01c7c3bac1cc1b
Yevhenii Bykov <yevhenii_bykov@epam.com>
2020-10-04 13:51:43 +0300

    added image as watermark

--- register.py
+++ register.py
@@ -24,6 +24,20 @@ def watermark_text(input_image_path,
     photo.save(output_image_path)
 
 
+def watermark_with_transparency(input_image_path,
+                                output_image_path,
+                                watermark_image_path,
+                                position):
+    base_image = Image.open(input_image_path)
+    watermark = Image.open(watermark_image_path)
+    width, height = base_image.size
+
+    transparent = Image.new('RGBA', (width, height), (0,0,0,0))
+    transparent.paste(base_image, (0,0))
+    transparent.paste(watermark, position, mask=watermark)
+    transparent.save(output_image_path)
+
+
 if __name__ == '__main__':
     os.system('clear')
     print('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░')
@@ -34,29 +48,65 @@ if __name__ == '__main__':
     print('░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║░░██║██║░░██║██║░╚██╗░')
     print('░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░')
     print('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░')
+
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     print('{0} | Developer: Daniil Mironyk'.format(dt_string))
-    print('{0} | Version: 1.0.3'.format(dt_string))
+    print('{0} | Version: 1.0.4'.format(dt_string))
     print('{0} | Required packages: pillow tkinter'.format(dt_string))
-    Tk().withdraw()
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print('{0} |'.format(dt_string), end=' ')
-    img = askopenfilename(filetypes=[('PNG pictures', '*.png'), ('JPEG pictures', '*.jpg')],
-                          title="Please select a Image")
-    print('Input file: {0}'.format(img))
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print('{0} |'.format(dt_string), end=' ')
-    ans = input("Watermark text: ")
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print('{0} |'.format(dt_string), end=' ')
-    output_file = input("Output file name: ") + '.png'
-    watermark_text(img, output_file, text=ans)
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print('{0} |'.format(dt_string), end=' ')
-    pwd = os.getcwd()
-    print('Saved file: {0}/{1}'.format(pwd, output_file))
+
+    choice_image = {'image'}
+    choice_text = {'text'}
+    print('{0} | Do you want to add a watermark as "text" or "image":'.format(dt_string), end=' ')
+    choice = input().lower()
+    if choice in choice_image:
+        Tk().withdraw()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        img_input = askopenfilename(filetypes=[('JPEG pictures', '*.jpg'), ('PNG pictures', '*.png')],
+                              title="Please select a Image")
+        print('Image: {0}'.format(img_input))
+        Tk().withdraw()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        watermark_img = askopenfilename(filetypes=[('PNG pictures', '*.png')],
+                              title="Please select a Watermark")
+        print('Watermark: {0}'.format(watermark_img))
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        output_file = input("Output file name: ") + '.png'
+        watermark_with_transparency(img_input, output_file, watermark_img, position=(0,0))
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        pwd = os.getcwd()
+        print('Saved file: {0}/{1}'.format(pwd, output_file))
+    elif choice in choice_text:
+        Tk().withdraw()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        img = askopenfilename(filetypes=[('JPEG pictures', '*.jpg'), ('PNG pictures', '*.png')],
+                              title="Please select a Image")
+        print('Input file: {0}'.format(img))
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        ans = input("Watermark text: ")
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        output_file = input("Output file name: ") + '.png'
+        watermark_text(img, output_file, text=ans)
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('{0} |'.format(dt_string), end=' ')
+        pwd = os.getcwd()
+        print('Saved file: {0}/{1}'.format(pwd, output_file))
+    else:
+      sys.stdout.write("Please respond with 'image' or 'text'")
+
+
